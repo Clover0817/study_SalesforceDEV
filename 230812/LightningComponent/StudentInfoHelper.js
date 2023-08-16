f8e936d790271154c4af32957509a5f1db768d25
Nayeon Kim <skdus3373@gmail.com>
2023-08-16 18:20:00 +0900

    Create StudentInfoHelper.js

---
+++ 230812/LightningComponent/StudentInfoHelper.js
@@ -0,0 +1,29 @@
+({
+    getInitData : function(component) {
+        var action = component.get("c.getInitData");
+
+        action.setParams({
+            recordId : component.get("v.recordId")
+        });
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            if(state === "SUCCESS") {
+                var returnValue = response.getReturnValue();
+                console.log(returnValue);
+                var objStudent = {
+                    'LastName' : returnValue['LastName'],
+                    'ClassName' : returnValue['ClassName']
+                };
+                component.set("v.objStudent", objStudent);
+            } else if(state === "ERROR") {
+                var errors = response.getError();
+                if(errors) {
+                    //참고 :에러가 났을경우는 주로 ShowToast 함수를 이용하여 토스트 메시지를 띄움
+                } else {
+                    
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+});
