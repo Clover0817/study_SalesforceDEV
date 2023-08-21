1bb1cbe24d9a7a0e9ea5f17c39c04d5396da3923
Nayeon Kim <skdus3373@gmail.com>
2023-08-21 16:18:38 +0900

    Update SearchStudentController.js

--- 230821/LightningComponent/SearchStudentController.js
+++ 230821/LightningComponent/SearchStudentController.js
@@ -1,30 +1,31 @@
+/**
+ * Created by skdus on 2023-08-21.
+ */
+
 ({
-    doInit: function(component, event, helper) {
-        component.set('v.columns', [
-            {label: '학생 정보', fieldName: 'Id', type: 'url', typeAttributes: {label: {fieldName: 'Name'}, target: '_blank'}},
-            {label: '학년', fieldName: 'Grade__c', type: 'text'},
-            {label: '반', fieldName: 'ClassNumber__c', type: 'text'}
-        ]);
-    },
-    
-    searchStudents: function(component, event, helper) {
-        var searchParams = component.get("v.searchParams");
-        
-        if (!searchParams.grade && !searchParams.classNumber && !searchParams.name) {
+    handleSearch: function(component, event, helper) {
+        var grade = component.get("v.grade");
+        var classNumber = component.get("v.classNumber");
+        var name = component.get("v.name");
+
+        if (!grade && !classNumber && !name) {
             helper.showToast("error", "조건을 입력해 주세요.");
-            return;
+        } else {
+            helper.searchStudents(component, grade, classNumber, name);
         }
-        
-        helper.searchStudents(component);
     },
-    
-    navigateToDetail: function (component, event, helper) {
-        var row = event.getParam('row');
-        var navEvt = $A.get("e.force:navigateToSObject");
-        navEvt.setParams({
-            "recordId": row.Id,
-            "slideDevName": "detail"
-        });
-        navEvt.fire();
-    }
+
+    navigateToRecordDetail: function(component, event, helper) {
+            var action = event.getParam('action');
+            var row = event.getParam('row');
+            var recordId = row.Id;
+
+            if (action.name === 'navigate') {
+                var navEvt = $A.get("e.force:navigateToSObject");
+                navEvt.setParams({
+                    "recordId": recordId
+                });
+                navEvt.fire();
+            }
+        }
 })
