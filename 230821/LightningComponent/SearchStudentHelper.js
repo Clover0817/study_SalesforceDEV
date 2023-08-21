74de196387e8e9ed9dd4489cf44a0263166fd9e9
Nayeon Kim <skdus3373@gmail.com>
2023-08-21 16:19:00 +0900

    Update SearchStudentHelper.js

--- 230821/LightningComponent/SearchStudentHelper.js
+++ 230821/LightningComponent/SearchStudentHelper.js
@@ -1,35 +1,40 @@
+/**
+ * Created by skdus on 2023-08-21.
+ */
+
 ({
-    searchStudents: function(component) {
+    searchStudents: function(component, grade, classNumber, name) {
         var action = component.get("c.searchStudents");
-        var searchParams = component.get("v.searchParams");
-        
+
         action.setParams({
-            grade: searchParams.grade,
-            classNumber: searchParams.classNumber,
-            name: searchParams.name
+            grade: grade,
+            classNumber: classNumber,
+            name: name
         });
-        
         action.setCallback(this, function(response) {
             var state = response.getState();
             if (state === "SUCCESS") {
-                var result = response.getReturnValue();
-                if (result.length === 0) {
-                    this.showToast("info", "검색된 내역이 없습니다.");
-                }
-                component.set("v.studentList", result);
-                component.set("v.totalRecords", result.length);
+                var students = response.getReturnValue();
+                console.log(students);
+                component.set('v.columns', [
+                    {label: '학생 정보', fieldName: 'Id', type: 'url'},
+                    {label: '학년', fieldName: 'Grade', type: 'number'},
+                    {label: '반', fieldName: 'ClassNumber', type: 'number'},
+                    {label: '이름', fieldName: 'Name', type: 'text'}
+                ]);
+                component.set("v.students", students);
             } else if (state === "ERROR") {
-                this.showToast("error", "오류가 발생하였습니다.");
+                this.toast("error", "오류가 발생했습니다");
             }
         });
         $A.enqueueAction(action);
     },
-    
+
     showToast: function(type, message) {
         var toastEvent = $A.get("e.force:showToast");
         toastEvent.setParams({
-            "type": type,
-            "message": message
+            type: type,
+            message: message
         });
         toastEvent.fire();
     }
